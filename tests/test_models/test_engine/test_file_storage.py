d4843c7462942fff242d2ec5d3a71e0fa88f2958
Blxee <aminedev57@gmail.com>
2023-08-12 18:14:14 +0100

    Added: more tests for FileSyorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -23,6 +23,17 @@ class TestFileStorage(unittest.TestCase):
                          + ' positional argument but 2 were given')
         self.assertEqual(storage.all(), storage._FileStorage__objects)
 
+    def test_new(self):
+        storage = FileStorage()
+        with self.assertRaises(TypeError) as err:
+            storage.new()
+        self.assertEqual(str(err.exception), 'FileStorage.new() missing 1'
+                         + " required positional argument: 'obj'")
+        with self.assertRaises(TypeError) as err:
+            storage.new("obj", "exess")
+        self.assertEqual(str(err.exception), 'FileStorage.new() takes 2'
+                         + ' positional arguments but 3 were given')
+
     def test_save(self):
         storage = FileStorage()
         with self.assertRaises(TypeError) as err:
@@ -32,3 +43,10 @@ class TestFileStorage(unittest.TestCase):
         storage.save()
         file_name = storage._FileStorage__file_path
         self.assertTrue(path.exists(file_name))
+
+    def test_reload(self):
+        storage = FileStorage()
+        with self.assertRaises(TypeError) as err:
+            storage.reload("exess")
+        self.assertEqual(str(err.exception), 'FileStorage.reload() takes 1'
+                         + ' positional argument but 2 were given')
